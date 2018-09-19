48c0dd394ba6c2e1be1e6200a482324940a11984
Ville Pietarinen <ville.pietarinen@gmail.com>
2018-09-20 00:13:31 +0300

    Add example

--- server.js
+++ server.js
@@ -3,12 +3,24 @@ const express = require('express');
 const app = express();
 const port = 5050;
 
+
 app.get('/', (req, res) => {
   console.log('Request arrived. Headers:');
   console.log(req.headers);
-  res.send('Föllari status lite');
+  res.send(`<h1>Föllari status lite</h1>\n<p>${timestamp}</p>`);
 });
 
-app.listen(port, function () {
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+// Example of we can request Föli's API every minute
+// using Node.js' event loop. We could extend EventEmitter
+// and implement it as a proper event when we have new data.
+let timestamp = '';
+
+const setTimestamp = () => {
+  timestamp = Math.round(Date.now() / 1000);
+};
+
+setInterval(setTimestamp, 1000);
