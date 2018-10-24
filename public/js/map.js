1d2a023d890646813f21aa92291a64547a32799a
Ville Pietarinen <ville.pietarinen@edu.turkuamk.fi>
2018-10-24 12:39:38 +0300

    Ditch Node for now, begin building clientside map

--- public/js/map.js
+++ public/js/map.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-    var mymap = L.map('mapid').setView([51.505, 0.09], 13);
+    let centerCoords = L.latLng(60.476, 22.138);
+
+    let mymap = L.map('mapid').setView(centerCoords, 13);
+
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
         maxZoom: 18
