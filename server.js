6edc56d5193e72128a97bf7284eae0e38836d014
Ville Pietarinen <ville.pietarinen@edu.turkuamk.fi>
2018-11-03 09:54:11 +0200

    Initial Node.JS web server using Express framework

--- server.js
+++ server.js
@@ -1,1 +1,43 @@
-// Web server
+const config = require('./readConfig');
+const openRouteService = require('./fetch-openroute');
+const express = require('express');
+const path = require('path');
+
+// Load configs
+const configs = config.readConfig('./config.json');
+
+// Fetch initial route
+let route;
+openRouteService.fetchRoute(configs.openrouteservice.apikey, configs.route.pointA, configs.route.pointB)
+    .then((value) => {
+        console.log('Fetched route from OpenRouteService successfully.');
+        route = value;
+    })
+    .catch((reason) => {
+        console.log('Error while fetching route from OpenRouteService:', reason);
+    });
+
+// Initialize Express
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res, next) => {
+    let options = {
+        root: path.join(__dirname, '/public/')
+    };
+
+    res.sendFile('index.html', options, (err) => {
+        if (err) next(err);
+        else console.log('Sent index.html');
+    });
+});
+
+app.get('/route', (req, res, next) => {
+    if (route) res.json(route);
+    else res.send(404, 'Error while loading route.');
+});
+
+app.listen(configs.webserver.port, () => {
+    console.log(`Web server is listening on port ${configs.webserver.port}.`);
+});
