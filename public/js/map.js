c342008930c3980dc16e1db8e87034ab95fa391a
Ville Pietarinen <ville.pietarinen@edu.turkuamk.fi>
2018-11-16 05:53:31 +0200

    Move route layer creation after handling Promise, add initial marker creation for POIs

--- public/js/map.js
+++ public/js/map.js
@@ -31,26 +31,42 @@ $(document).ready(() => {
   let map = loadMap('mapid');
 
   let busRoute = getServiceData()
-    .then((data) => {
-      console.log('Service data loaded successfully');
-      console.log(data);
+  .then((data) => {
+    console.log('Service data loaded successfully');
+    console.log(data);
 
-      // Add route to map
-      let options = {
-        color:        '#EBAC00',
-        interactive:  false,
-        weight:       5
-      };
-
-      let routePolyline = L.polyline(data.route, options);
-      routePolyline.addTo(map);
-      map.fitBounds(routePolyline.getBounds());
-
-      return data;
+    return data;
   })
   .catch((reason) => {
     let errorMessage = 'Error while loading service data from server';
     console.log(errorMessage);
     $('#mapid').html(errorMessage);
-  });
+  })
+  .then((data) => {
+    // Add route layer to map
+    let options = {
+      color:        '#EBAC00',
+      interactive:  false,
+      weight:       5
+    };
+
+    let routePolyline = L.polyline(data.route, options);
+    routePolyline.addTo(map);
+    map.fitBounds(routePolyline.getBounds());
+
+    // Create marker layers
+    let markers = {};
+
+    for (let point of data.pointsOfInterest) {
+      let pointMarker = L.marker(L.latLng(point.lat, point.lon), {icon: L.icon({iconUrl: 'img/'+point.category+'.png', iconSize: L.point(26, 26)})});
+      // remember to fix popup info
+      pointMarker.bindPopup(JSON.stringify(point, null, 2));
+      // DEBUG addTo -> IMPLEMENT FEATUREGROUPS & CONTROL LAYER GODDAMIT
+      pointMarker.addTo(map);
+      if (!markers.hasOwnProperty(point.category)) { markers[point.category] = []; }
+      markers[point.category].push(pointMarker);
+    }
+
+    console.log(markers);
+  })
 });
